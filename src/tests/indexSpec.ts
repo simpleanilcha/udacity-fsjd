c634dc78070020e496a467d8991ebdf0c4aa0641
anil.maharjan@chuchuro.com <simpleanilcha@gmail.com>
2021-05-08 00:31:12 +0545

    add describe in test

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,7 +1,7 @@
 import myFunc from '../index'
 
-// describe("suite description", () => {
+describe("suite description", () => {
   it('expect myFunc(5) to equal 25', () => {
     expect(myFunc(5)).toEqual(25);
   });
-// })
+})
