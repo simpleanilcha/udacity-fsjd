ff0c1f4bd28b8fffb165758b812c35b35cd9b949
anil.maharjan@chuchuro.com <simpleanilcha@gmail.com>
2021-05-08 14:33:47 +0545

    solve exercise test driven development

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,53 @@
-import arrays from './utilities/arrays.js';
-import numbers from './utilities/numbers.js';
-import strings from './utilities/strings.js';
+/** Use Axios to get data from restcountries api */
+import axios from 'axios';
 
-const numArr = [3, 4, 5, 6];
-const wordArr = ['cat', 'dog', 'rabbit', 'bird'];
-const arrSum = arrays.addArr(numArr);
-const mixArr = arrays.concatArr(numArr, wordArr);
-const myNum = ('15' as unknown) as number % 2;
-const five = parseInt('5');
+/** Use the free API https://restcountries.eu/
+ * You will use the following endpoints
+ * https://restcountries.eu/rest/v2/name/{name} for countries by name
+ * https://restcountries.eu/rest/v2/regionalbloc/{regionalbloc} for region blocks
+ */
+
+/** Create getCountry Function here */
+async function getCountry(countryName: string) {
+  const getApi = await axios(`https://restcountries.eu/rest/v2/name/${countryName}`);
+  const data = getApi.data[0];
+  return {
+    capital: data.capital,
+    region: data.region,
+    numericCode: data.numericCode
+  }
+}
+
+/** Create a test for this getRegion function */
+async function getRegionCountries(regionalbloc: string) {
+  const getApi = await axios(
+    `https://restcountries.eu/rest/v2/regionalbloc/${regionalbloc}`
+  );
+  const data = getApi.data;
+  const countries = [];
+  for (let i = 0; i < data.length; i++) {
+    countries.push(data[i].name);
+  }
+  return countries;
+}
+
+/** Create getRegionCapitals function here */
+async function getRegionCapitals(regionalbloc: string) {
+  const countryEndpoints = await getRegionCountries(regionalbloc);
+  const promises = countryEndpoints.map((endpoint: any) => 
+    axios(`https://restcountries.eu/rest/v2/name/${endpoint}`)
+  );
+  const arr = await Promise.all(promises);
+  const capitals = [];
+  for (let i = 0; i < arr.length; i++) {
+    capitals.push(arr[i].data[0].capital);
+  }
+  return capitals;
+}
 
 
-console.log(arrays.cut3(mixArr));
-console.log(numbers.sum(arrSum, myNum));
-console.log(strings.capitalize('the quick brown fox'));
-console.log(numbers.multiply(five, 8));
-console.log(arrays.lgNum(mixArr));
+export default {
+  getCountry,
+  getRegionCountries,
+  getRegionCapitals
+};
--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,51 +1,23 @@
-import arrays from '../utilities/arrays';
-import numbers from '../utilities/numbers.js';
-import strings from '../utilities/strings.js';
+import countries from "../index";
 
-const numArr = [3, 4, 5, 6]
-const wordArr = ['cat', 'dog', 'rabbit', 'bird']
+it("should get basic data on the country canada", async () => {
+	const data = await countries.getCountry('canada');
+	expect(data).toEqual({
+		capital: 'Ottawa',
+		region: 'Americas',
+		numericCode: '124'
+	});
+});
 
-// test for arrays
-describe("Arrays suite test", () => {
-  it('should add numbers in array and be truthy', () => {
-    expect(arrays.addArr(numArr)).toBeTruthy();
-  });
-
-  it('sholud concatinate 2 arrays to not equal just 1', () => {
-    expect(arrays.concatArr(numArr, wordArr)).not.toEqual(numArr);
-  })
-
-  it('should not contain the third index', () => {
-    expect(arrays.cut3(wordArr)).not.toContain('rabbit');
-  })
-
-  it('should no have a large number and be falsy', () => {
-    expect(arrays.lgNum(wordArr)).toBeFalsy();
-  })
+/** Add test for getRegionCountries function here */
+it("should get the countries in the region NAFTA", async () => {
+  const data = await countries.getRegionCountries('nafta');
+  expect(data).toEqual(['Canada', 'Mexico', 'United States of America']);
 })
 
-// test for numbers
-describe("Numbers suite test", () => {
-  it('expect multiply 5 and 2 to equal 10', () => {
-    expect(numbers.multiply(5, 2)).toBe(10);
-  });
-
-  it('expect sum 5 and 12 to >10', () => {
-    expect(numbers.sum(5, 12)).toBeGreaterThan(10);
-  });
-
-  it('expect divide 5 and 10 to <10', () => {
-    expect(numbers.divide(5, 10)).toBeLessThan(10);
-  });
-
-  it('expect square of 5 to >=25', () => {
-    expect(numbers.square(5)).toBeGreaterThanOrEqual(25);
-  });
-})
-
-// test for strings
-describe("Strings suite test", () => {
-  it('should capitalize a string', () => {
-    expect(strings.capitalize('a sentence')).toEqual('A Sentence');
-  });
-})
+it("should get capitals of NAFTA countries", async () => {
+	const data = await countries.getRegionCapitals('nafta');
+	expect(data).toEqual([
+		'Ottawa', 'Mexico City', 'Washington, D.C.'
+	]);
+});
